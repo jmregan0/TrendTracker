7deadb1d06a7ac8920125153e931aa2a11ce502f
Jacob Regan <jacobmregan0@gmail.com>
2017-06-17 15:01:05 -0500

    Connects app server and server connected to twitter api routes through cross origin. Renders global data correctly

---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const chalk = require('chalk');
+// const moment = require('moment');
+const axios = require('axios');
+
+const app = express();
+const twitterBearerToken = /*process.env.TWITTER_BEARER_TOKEN ?
+                           process.env.TWITTER_BEARER_TOKEN :*/
+                           require('./secrets').bearer_token_creds;
+
+
+
+app.use((req, res, next) =>{
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X_Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/:place', (req, res, next) => {
+    axios({
+      method: 'post',
+      url: 'https://api.twitter.com/oauth2/token',
+      headers: {
+        Authorization: `Basic ${new Buffer(twitterBearerToken).toString('base64')}`,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+      data: 'grant_type=client_credentials',
+    })
+    .then(res => res.data)
+    .then(({ access_token: accessToken }) => {
+      app.use(morgan('dev'));
+      app.use(express.static('public'));
+
+    if(req.params.place === 'global'){
+      axios({
+          method: 'get',
+          url: 'https://api.twitter.com/1.1/trends/place.json?id=1',
+          headers: { Authorization: `Bearer ${accessToken}`}
+        })
+        .then(response => res.send(response.data[0]))
+    }
+    else if(req.params.place === 'chicago'){
+      axios({
+        method: 'get',
+        url: 'https://api.twitter.com/1.1/trends/place.json?id=2379574',
+        headers: { Authorization: `Bearer ${accessToken}`}
+      })
+      .then(response => res.send(response))
+    }
+    else if(req.params.place === 'nyc'){
+      axios({
+        method: 'get',
+        url: 'https://api.twitter.com/1.1/trends/place.json?id=2459115',
+        headers: { Authorization: `Bearer ${accessToken}`}
+      })
+      .then(response => res.send(response))
+    }
+    else if(req.params.place === 'la'){
+      axios({
+        method: 'get',
+        url: 'https://api.twitter.com/1.1/trends/place.json?id=2442047',
+        headers: { Authorization: `Bearer ${accessToken}`}
+      })
+      .then(response => res.send(response))
+    }
+  })
+})
+
+
+
+
+
+
+app.use((err, req, res, next) =>{
+  if(err){
+    console.error(err)
+    res.end()
+  } else {
+    console.log(req)
+    res.end()
+  }
+})
+
+app.listen(3001, console.log('listening on 3001 all day baby!'))
---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react'
+import './App.css'
+import * as d3 from 'd3'
+import './App.css'
+import BubbleChart from './components/BubbleChart'
+import Bubbles from './components/Bubbles'
+import YearsTitles from './components/YearsTitles'
+import GroupingPicker from './components/GroupingPicker'
+import { createNodes } from './utils'
+import { width, height, center, yearCenters } from './constants'
+import axios from 'axios'
+
+export default class App extends Component {
+  state = {
+    data: [],
+    grouping: 'all',
+  }
+
+  componentDidMount() {
+
+    /*
+
+    Instead of setting up another server running on a separate port to be used as a proxy,
+    using axios request from within this componentDidMount() to my other server running on
+    localhost:3001. This will serve up api routes for twitter calls / auth.
+
+    On app.js (server code for handling twitter stuff) I make calls for bearer token and
+    twitter stream json. Could tear that part out of the project and put it in its own file.
+    Definitely don't want token info on any public facing code.
+
+    app.js server code for twitter calls needs header middleware to enable cross origin requests
+    from main app running on 3000.
+
+    */
+
+    axios({
+      method: 'get',
+      url: 'http://localhost:3001/global'
+    })
+    .then(result => {
+      this.setState({
+          data: createNodes(result),
+        })
+    })
+  }
+
+
+  onGroupingChanged = (newGrouping) => {
+    this.setState({
+      grouping: newGrouping,
+    })
+  }
+
+  render() {
+    const { data, grouping } = this.state
+    return (
+      <div className="App">
+        <GroupingPicker onChanged={this.onGroupingChanged} active={grouping} />
+        <BubbleChart width={width} height={height}>
+          <Bubbles data={data} forceStrength={0.03} center={center} yearCenters={yearCenters} groupByYear={grouping === 'year'} />
+          {
+            grouping === 'year' &&
+            <YearsTitles width={width} yearCenters={yearCenters} />
+          }
+        </BubbleChart>
+      </div>
+    )
+  }
+
+}
